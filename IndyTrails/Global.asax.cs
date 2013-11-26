a3e0173f2ce5f7b41a4d5f2d621d9efefa999abc
Nick Kirkes <nick.kirkes@gmail.com>
2013-11-26 18:56:09 -0500

    cleaned up routing to support ext login call backs; added mdf/ldf to .gitignore.

--- IndyTrails/Global.asax.cs
+++ IndyTrails/Global.asax.cs
@@ -14,6 +14,7 @@ namespace IndyTrails
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
